344fef6fcb783dad8b7fb033927b400120ea9363
Tomas Balaz <balaztomas0@gmail.com>
2023-02-01 14:03:01 +0100

    Exposed REST API /api/v1/notifications/{id}

--- src/main/java/sk/balaz/springbootmongowebflux/notification/NotificationController.java
+++ src/main/java/sk/balaz/springbootmongowebflux/notification/NotificationController.java
@@ -2,9 +2,11 @@ package sk.balaz.springbootmongowebflux.notification;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/v1/notifications")
@@ -16,5 +18,10 @@ public class NotificationController {
     @GetMapping
     public Flux<Notification> getAllNotifications() {
         return notificationService.getAllNotifications();
+    }
+
+    @GetMapping("{notificationId}")
+    public Mono<Notification> getNotificationById(@PathVariable("notificationId") String notificationId) {
+        return notificationService.getNotificationById(notificationId);
     }
 }
--- src/main/java/sk/balaz/springbootmongowebflux/notification/NotificationService.java
+++ src/main/java/sk/balaz/springbootmongowebflux/notification/NotificationService.java
@@ -3,6 +3,7 @@ package sk.balaz.springbootmongowebflux.notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +13,9 @@ public class NotificationService {
 
     public Flux<Notification> getAllNotifications() {
         return notificationRepository.findAll();
+    }
+
+    public Mono<Notification> getNotificationById(String id) {
+        return notificationRepository.findById(id);
     }
 }
